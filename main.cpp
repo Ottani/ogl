490a0aba5c89c0abe1851f55146432b34ecf3d0b
Paulo Assis <paulo.ottani@yahoo.ca>
2017-09-07 12:04:59 -0300

    events

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+using std::cout;
+using std::cerr;
+
+// TODO remove globals
+int g_width = 640;
+int g_height = 480;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main(void)
 {
@@ -12,22 +24,50 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(g_width, g_height, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
+	
+	if (GLenum err = glewInit(); err = GLEW_OK)
+	{
+		cerr << "Error: " << glewGetErrorString(err) << '\n';
+		return -2;
+	}
+
+	glViewport(0, 0, g_width, g_height);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
+		processInput(window);
+		
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+        
         glfwPollEvents();
+
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	g_width = width;
+	g_height = height;
+    glViewport(0, 0, width, height);
+    cout << "W: " << width << " H: " << height << '\n';
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
